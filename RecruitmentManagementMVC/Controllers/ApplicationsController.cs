b6425ed2e9a7944ce6df3887f73a7e97a889f36c
Nguyen Thi Kim Van <vannguyentk850904@gmail.com>
2025-10-17 18:48:10 +0700

    buoc 2 ket noi database va tao cac controllers, models, views

---
+++ RecruitmentManagementMVC/Controllers/ApplicationsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RecruitmentManagementMVC.Models;
+
+namespace RecruitmentManagementMVC.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private RecruitmentDbEntities db = new RecruitmentDbEntities();
+
+        // GET: Applications
+        public ActionResult Index()
+        {
+            var applications = db.Applications.Include(a => a.User).Include(a => a.JobPost);
+            return View(applications.ToList());
+        }
+
+        // GET: Applications/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Create
+        public ActionResult Create()
+        {
+            ViewBag.CandidateId = new SelectList(db.Users, "UserId", "FullName");
+            ViewBag.JobId = new SelectList(db.JobPosts, "JobId", "Title");
+            return View();
+        }
+
+        // POST: Applications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ApplicationId,JobId,CandidateId,CoverLetter,AppliedAt")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Applications.Add(application);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CandidateId = new SelectList(db.Users, "UserId", "FullName", application.CandidateId);
+            ViewBag.JobId = new SelectList(db.JobPosts, "JobId", "Title", application.JobId);
+            return View(application);
+        }
+
+        // GET: Applications/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CandidateId = new SelectList(db.Users, "UserId", "FullName", application.CandidateId);
+            ViewBag.JobId = new SelectList(db.JobPosts, "JobId", "Title", application.JobId);
+            return View(application);
+        }
+
+        // POST: Applications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ApplicationId,JobId,CandidateId,CoverLetter,AppliedAt")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(application).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CandidateId = new SelectList(db.Users, "UserId", "FullName", application.CandidateId);
+            ViewBag.JobId = new SelectList(db.JobPosts, "JobId", "Title", application.JobId);
+            return View(application);
+        }
+
+        // GET: Applications/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // POST: Applications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Application application = db.Applications.Find(id);
+            db.Applications.Remove(application);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RecruitmentManagementMVC/Controllers/JobPostsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RecruitmentManagementMVC.Models;
+
+namespace RecruitmentManagementMVC.Controllers
+{
+    public class JobPostsController : Controller
+    {
+        private RecruitmentDbEntities db = new RecruitmentDbEntities();
+
+        // GET: JobPosts
+        public ActionResult Index()
+        {
+            var jobPosts = db.JobPosts.Include(j => j.User);
+            return View(jobPosts.ToList());
+        }
+
+        // GET: JobPosts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobPost jobPost = db.JobPosts.Find(id);
+            if (jobPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobPost);
+        }
+
+        // GET: JobPosts/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployerId = new SelectList(db.Users, "UserId", "FullName");
+            return View();
+        }
+
+        // POST: JobPosts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "JobId,EmployerId,Title,Description,Location,Salary,CreatedAt")] JobPost jobPost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.JobPosts.Add(jobPost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployerId = new SelectList(db.Users, "UserId", "FullName", jobPost.EmployerId);
+            return View(jobPost);
+        }
+
+        // GET: JobPosts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobPost jobPost = db.JobPosts.Find(id);
+            if (jobPost == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployerId = new SelectList(db.Users, "UserId", "FullName", jobPost.EmployerId);
+            return View(jobPost);
+        }
+
+        // POST: JobPosts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "JobId,EmployerId,Title,Description,Location,Salary,CreatedAt")] JobPost jobPost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobPost).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployerId = new SelectList(db.Users, "UserId", "FullName", jobPost.EmployerId);
+            return View(jobPost);
+        }
+
+        // GET: JobPosts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobPost jobPost = db.JobPosts.Find(id);
+            if (jobPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobPost);
+        }
+
+        // POST: JobPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            JobPost jobPost = db.JobPosts.Find(id);
+            db.JobPosts.Remove(jobPost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
